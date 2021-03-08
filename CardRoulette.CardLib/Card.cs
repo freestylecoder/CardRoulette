9739aa6db326d2c07fa2f7071b0fe77e8662b416
Chris Gardner <cgardner@gmail.com>
2021-03-08 13:59:06 -0600

    Stubbed out starting of the card library

---
+++ CardRoulette.CardLib/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardRoulette.CardLib {
+	public class Card {
+	}
+}
+ /*
+ 0yyyyxxx
+
+xxx = suit
+100 = Joker
+011 = Spades
+010 = Hearts
+001 = Diamonds
+000 = Clubs
+
+yyyy = rank
+0010 = 2
+...
+0110 = 10
+
+1000 = Jack
+1001 = Queen
+1010 = King
+1011 = Ace
+
+1100 = Small
+1101 = Big
+ */
