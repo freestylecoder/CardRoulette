fbff3ed751af9cc7456b88cee581979acb93e2e3
Chris Gardner <cgardner@gmail.com>
2021-11-08 14:01:21 -0600

    Most adding and removing bests are working

---
+++ CardRoulette/Bet.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace CardRoulette {
+	public class Bet {
+		public int Amount;
+		public Vector2 Position;
+
+		public Bet( int amount, Vector2 position ) {
+			this.Amount = amount;
+			this.Position = position;
+		}
+	}
+}
--- CardRoulette/Game1.cs
+++ CardRoulette/Game1.cs
@@ -15,12 +15,13 @@ namespace CardRoulette {
 		private Deck DeckOfCards;
 		private Deck DiscardPile;
 
+		private int PlayerChips;
+		private IList<Bet> Bets;
 		private Vector2 CursorPosition;
 		private IEnumerable<Vector2> Points;
 
 		private Texture2D Cursor;
 		private Texture2D FullBoard;
-		private Texture2D WhiteSquare;
 
 		private SpriteFont DefaultFont;
 
@@ -39,6 +40,9 @@ namespace CardRoulette {
 
 		protected override void Initialize() {
 			// TODO: Add your initialization logic here
+
+			PlayerChips = 100;
+			Bets = new List<Bet>();
 			DeckOfCards = new Deck();
 			DiscardPile = Deck.PokerDeck( false );
 			LastKeyboardState = Keyboard.GetState();
@@ -55,7 +59,6 @@ namespace CardRoulette {
 
 			Cursor = Content.Load<Texture2D>( "Cursor" );
 			FullBoard = Content.Load<Texture2D>( "FullBoard" );
-			WhiteSquare = Content.Load<Texture2D>( "WhiteSquare" );
 
 			Color[] colors = new Color[FullBoard.Width*FullBoard.Height];
 			FullBoard.GetData( colors );
@@ -74,7 +77,6 @@ namespace CardRoulette {
 			);
 
 			CursorPosition = Points.First();
-			//CursorPosition = new Vector2( 360 - 12.5F, 340 - 12.5F );
 		}
 
 		protected override void Update( GameTime gameTime ) {
@@ -84,16 +86,46 @@ namespace CardRoulette {
 			Func<Keys,bool> KeyWasPressed = ( key ) =>
 				Keyboard.GetState().IsKeyDown( key ) && LastKeyboardState.IsKeyUp( key );
 
-			Func<IEnumerable<Vector2>,Vector2> FindClosest = ( possible ) =>
-				possible.Any()
+			Func<bool> ShiftDown = () =>
+				Keyboard.GetState()
+					.GetPressedKeys()
+					.Intersect( new[] { Keys.LeftShift, Keys.RightShift } )
+					.Any();
+
+			Action<int> UpdateBets = ( betAmount ) => {
+				Bet bet = Bets.FirstOrDefault( b => b.Position == CursorPosition );
+				if( null == bet ) {
+					if( !ShiftDown() ) {
+						if( PlayerChips >= betAmount ) {
+							PlayerChips -= betAmount;
+							Bets.Add( new Bet( betAmount, CursorPosition ) );
+						}
+					}
+				} else {
+					if( ShiftDown() ) {
+						PlayerChips += Math.Min( bet.Amount, betAmount );
+						bet.Amount -= betAmount;
+						if( bet.Amount < 1 ) Bets.Remove( bet );
+					} else {
+						if( PlayerChips >= betAmount ) {
+							PlayerChips -= betAmount;
+							bet.Amount += betAmount;
+						}
+					}
+				}
+			};
+
+			Vector2 FindClosest( IEnumerable<Vector2> possible ) {
+				return possible.Any()
 					? possible
-						.Select( p => ( Vector2.Distance( CursorPosition, p ), p ) )
+						.Select( p => (Vector2.Distance( CursorPosition, p ), p) )
 						.OrderBy( t => t.Item1 )
 						.First()
 						.Item2
 					: CursorPosition;
+			}
 
-			if( KeyWasPressed( Keys.Right ) ) {
+			if( KeyWasPressed( Keys.Right ) || KeyWasPressed( Keys.D ) ) {
 				CursorPosition = FindClosest(
 					Points.Any( p => p.Y == CursorPosition.Y && p.X > CursorPosition.X )
 						? Points
@@ -104,7 +136,7 @@ namespace CardRoulette {
 				);
 			}
 
-			if( KeyWasPressed( Keys.Left ) ) {
+			if( KeyWasPressed( Keys.Left ) || KeyWasPressed( Keys.A ) ) {
 				CursorPosition = FindClosest(
 					Points.Any( p => p.Y == CursorPosition.Y && p.X < CursorPosition.X )
 						? Points
@@ -115,7 +147,7 @@ namespace CardRoulette {
 				);
 			}
 
-			if( KeyWasPressed( Keys.Up ) ) {
+			if( KeyWasPressed( Keys.Up ) || KeyWasPressed( Keys.W ) ) {
 				CursorPosition = FindClosest(
 					Points.Any( p => p.X == CursorPosition.X && p.Y < CursorPosition.Y )
 						? Points
@@ -126,7 +158,7 @@ namespace CardRoulette {
 				);
 			}
 
-			if( KeyWasPressed( Keys.Down ) ) {
+			if( KeyWasPressed( Keys.Down ) || KeyWasPressed( Keys.S ) ) {
 				CursorPosition = FindClosest(
 					Points.Any( p => p.X == CursorPosition.X && p.Y > CursorPosition.Y )
 						? Points
@@ -137,7 +169,27 @@ namespace CardRoulette {
 				);
 			}
 
-			// TODO: Add your update logic here
+			// 1 - White - $1
+			if( KeyWasPressed( Keys.D1 ) || KeyWasPressed( Keys.NumPad1 ) ) UpdateBets( 1 );
+
+			// 2 - Red - $5
+			if( KeyWasPressed( Keys.D2 ) || KeyWasPressed( Keys.NumPad2 ) ) UpdateBets( 5 );
+
+			// 3 - Blue - $10
+			if( KeyWasPressed( Keys.D3 ) || KeyWasPressed( Keys.NumPad3 ) ) UpdateBets( 10 );
+
+			// 4 - Green - $25
+			if( KeyWasPressed( Keys.D4 ) || KeyWasPressed( Keys.NumPad4 ) ) UpdateBets( 25 );
+
+			if( KeyWasPressed( Keys.Z ) ) {
+				UpdateBets( PlayerChips );
+			}
+
+			if( KeyWasPressed( Keys.X ) ) {
+				Bet bet = Bets.LastOrDefault( b => b.Position == CursorPosition );
+				PlayerChips += bet?.Amount ?? 0;
+				Bets.Remove( bet );
+			}
 
 			//if( TimeSpan.FromMilliseconds( 100 ) > DrawTimer ) {
 			//	DrawTimer += gameTime.ElapsedGameTime;
@@ -217,9 +269,22 @@ namespace CardRoulette {
 
 			_spriteBatch.Begin();
 			_spriteBatch.Draw( FullBoard, boardCorner, Color.White );
-			_spriteBatch.Draw( Cursor, trueCursorPos, null, Color.White, 0f, cursorCenter, 1f, SpriteEffects.None, 1f );
 
-			_spriteBatch.DrawString( DefaultFont, trueCursorPos.ToString(), Vector2.Zero, Color.White );
+			foreach( Bet bet in Bets )
+				_spriteBatch.Draw( Cursor, bet.Position + boardCorner, null, Color.White, 0f, cursorCenter, 1f, SpriteEffects.None, 1f );
+
+			_spriteBatch.Draw( Cursor, trueCursorPos, null, Color.FromNonPremultiplied( 0, 255, 0, 127 ), 0f, cursorCenter, 1f, SpriteEffects.None, 1f );
+
+			//_spriteBatch.DrawString( DefaultFont, trueCursorPos.ToString(), Vector2.Zero, Color.White );
+			_spriteBatch.DrawString(
+				DefaultFont,
+				$@"Player Chips: {PlayerChips}
+Current Bet : {Bets.Where( b => b.Position == CursorPosition ).Sum( b => b.Amount )}
+Total Bets  : {Bets.Sum( b => b.Amount )}",
+				Vector2.Zero,
+				Color.White
+			);
+
 			_spriteBatch.End();
 
 			base.Draw( gameTime );
